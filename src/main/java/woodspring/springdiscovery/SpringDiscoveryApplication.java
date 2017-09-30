a8381391e0a9e2b3c25c2b5f0be79be4c90a5114
woodspring <woodspring.toronto@gmail.com>
2017-09-30 13:38:25 -0400

    Add files via upload
    Initialize Spring with Eureka

---
+++ src/main/java/woodspring/springdiscovery/SpringDiscoveryApplication.java
@@ -0,0 +1,14 @@
+package woodspring.springdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiscoveryApplication.class, args);
+	}
+}
